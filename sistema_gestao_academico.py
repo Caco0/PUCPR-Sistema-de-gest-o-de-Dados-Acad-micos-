34a3cbb8b6a6ef72962b4f643d336487c666d698
Caco0 <cacotpds@gmail.com>
2024-06-16 11:10:23 -0300

    "Simplified menu system by consolidating similar code into a single function and removed redundant menu definitions."

--- sistema_gestao_academico.py
+++ sistema_gestao_academico.py
@@ -1,15 +1,12 @@
 def menu_sistema_principal():
-    print(
-        """
-    ====================Sistema de gestão de Dados Acadêmicos====================
-    =============================================================================
-"""
-    )
     opcao = -1
     while opcao != 0:
         opcao = int(
             input(
                 """
+    ====================Sistema de gestão de Dados Acadêmicos=====================
+    ==============================================================================
+
     =============================Menu - Navegação=================================
      [0] Exit [1] Estudantes [2] Disciplinas [3] Professor [4] Turma [5] Matrícula
     ==============================================================================
@@ -18,15 +15,15 @@ def menu_sistema_principal():
         )
 
         if opcao == 1:
-            estudantes()
+            estudantes(opcao=1)
         elif opcao == 2:
-            disciplinas()
+            estudantes(opcao=2)
         elif opcao == 3:
-            professor()
+            estudantes(opcao=3)
         elif opcao == 4:
-            turma()
+            estudantes(opcao=4)
         elif opcao == 5:
-            matriculas()
+            estudantes(opcao=5)
         elif opcao < 0 or opcao > 5:
             print("Opção inválida")
         elif opcao == 0:
@@ -36,141 +33,43 @@ def menu_sistema_principal():
         print("Saindo do Sistema")
 
 
-def estudantes():
-    opcao = -1
-    while opcao != 0:
-        opcao = int(
-            input(
-                """
-    ===================Menu - Estudante========================
-     [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
-    ===========================================================
-    """
-            )
-        )
-        if opcao == 1:
-            print("Incluir Estudantes")
-        elif opcao == 2:
-            print("Listar Estudantes")
-        elif opcao == 3:
-            print("Atualizar Estudantes")
-        elif opcao == 4:
-            print("Excluir Estudantes")
-        elif opcao < 0 or opcao >= 5:
-            print("Opção inválida")
-        elif opcao == 0:
-            print("Saindo...")
+def estudantes(opcao):
+    if opcao == 1:
+        menu_nome = "Estudante"
+    elif opcao == 2:
+        menu_nome = "Disciplinas"
+    elif opcao == 3:
+        menu_nome = "Professor"
+    elif opcao == 4:
+        menu_nome = "Turma"
     else:
-        print("Voltando ao menu principal")
+        menu_nome = "Matrícula"
 
-
-def disciplinas():
-    opcao = -1
-    while opcao != 0:
-        opcao = int(
+    caso = -1
+    while caso != 0:
+        caso = int(
             input(
-                """
-    ===================Menu - Disciplinas=======================
-     [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
-    ===========================================================
+                f"""
+    ====================Sistema de gestão de Dados Acadêmicos=====================
+    ==============================================================================
+    
+                                Menu - {menu_nome}
+              [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
+    ==============================================================================
     """
             )
         )
-        if opcao == 1:
-            print("Incluir Disciplina")
-        elif opcao == 2:
-            print("Listar Disciplina")
-        elif opcao == 3:
-            print("Atualizar Disciplina")
-        elif opcao == 4:
-            print("Excluir Disciplina")
-        elif opcao < 0 or opcao >= 5:
-            print("Opção Inválida")
-        elif opcao == 0:
-            print("Saindo do menu Disciplinas")
-    else:
-        print("Voltando ao menu Principal")
-
-
-def professor():
-    opcao = -1
-    while opcao != 0:
-        opcao = int(
-            input(
-                """
-    ===================Menu - Professor========================
-     [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
-    ===========================================================
-    """
-            )
-        )
-        if opcao == 1:
-            print("Incluir Professor")
-        elif opcao == 2:
-            print("Listar Professor")
-        elif opcao == 3:
-            print("Atualizar Professor")
-        elif opcao == 4:
-            print("Excluir Professor")
-        elif opcao < 0 or opcao >= 5:
+        if caso == 1:
+            print(f"Incluir {menu_nome}")
+        elif caso == 2:
+            print(f"Listar {menu_nome}")
+        elif caso == 3:
+            print(f"Atualizar {menu_nome}")
+        elif caso == 4:
+            print(f"Excluir {menu_nome}")
+        elif caso < 0 or opcao >= 5:
             print("Opção inválida")
-        elif opcao == 0:
-            print("Saindo...")
-    else:
-        print("Voltando ao menu Principal")
-
-
-def turma():
-    opcao = -1
-    while opcao != 0:
-        opcao = int(
-            input(
-                """
-    =====================Menu - Turma==========================
-     [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
-    ===========================================================
-    """
-            )
-        )
-        if opcao == 1:
-            print("Incluir Turma")
-        elif opcao == 2:
-            print("Listar Turma")
-        elif opcao == 3:
-            print("Atualizar Turma")
-        elif opcao == 4:
-            print("Excluir Turma")
-        elif opcao < 0 or opcao >= 5:
-            print("Opção inválida")
-        elif opcao == 0:
-            print("Saindo...")
-    else:
-        print("Voltando ao menu Principal")
-
-
-def matriculas():
-    opcao = -1
-    while opcao != 0:
-        opcao = int(
-            input(
-                """
-    ====================Menu - Matrículas======================
-     [0] Voltar [1] Incluir [2] Listar [3] Atualizar [4] Excluir
-    ===========================================================
-    """
-            )
-        )
-        if opcao == 1:
-            print("Incluir Matrículas")
-        elif opcao == 2:
-            print("Listar Matrículas")
-        elif opcao == 3:
-            print("Atualizar Matrículas")
-        elif opcao == 4:
-            print("Excluir Matrículas")
-        elif opcao < 0 or opcao >= 5:
-            print("Opção inválida")
-        elif opcao == 0:
+        elif caso == 0:
             print("Saindo...")
     else:
         print("Voltando ao menu principal")
